3d73f3c2c77dbaad530836c5ce0196ba91ab9e05
Christian Sanders <christian.sanders@live.com>
2019-05-18 23:00:39 -0700

    Finish basic structure

--- src/chip8.rs
+++ src/chip8.rs
@@ -1,24 +1,36 @@
 pub struct Chip8 {
     // ram
     memory: [u8; 4096],
+    stack: [u8; 16],
+    sp: u8,
 
     // cpu
     registers: [u8; 16],
     i: u8,
     pc: u8,
+    delay_timer: u8,
+    sound_timer: u8,
 
     // display
-    gfx: [[i16; 64]; 32],
+    gfx: [[u8; 64]; 32],
+
+    // keyboard
+    key: [u8; 16],
 }
 
 impl Chip8 {
     pub fn new() -> Self {
         Chip8 {
             memory: [0; 4096],
+            stack: [0; 16],
+            sp: 0,
             registers: [0; 16],
             i: 0x000,
             pc: 0x000,
+            delay_timer: 0,
+            sound_timer: 0,
             gfx: [[0; 64]; 32],
+            key: [0; 16]
         }
     }
 
